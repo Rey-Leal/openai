169106f74d31e18be621c2b34ad9a03057ce9f25
ReyLeal <reinaldoleal2@gmail.com>
2023-11-08 10:33:49 -0300

    ChatGPT Funcional

--- chat.php
+++ chat.php
@@ -2,46 +2,89 @@
 // Chave privada e pessoal de acesso a API
 include_once("keys/apikey.php");
 
-try {
-    $entrada = "Qual a capital do Brasil?";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    try {
+        $entrada = $_POST["pergunta"];;
 
-    // ChatGPT
-    $url = 'https://api.openai.com/v1/chat/completions';
+        // ChatGPT - Respostas via Machine Learning 
+        $url = 'https://api.openai.com/v1/chat/completions';
 
-    $data = [
-        'messages' => [
-            [
-                'role' => 'system',
-                'content' => 'Você é um assistente que responde a perguntas.'
-            ],
-            [
-                'role' => 'user',
-                'content' => $entrada
-            ]
-        ]
-    ];
+        $data = array(
+            "messages" => array(
+                array(
+                    "role" => "system",
+                    "content" => "Você é um assistente que responde a perguntas sobre fatos."
+                ),
+                array(
+                    "role" => "user",
+                    "content" => "$entrada"
+                )
+            ),
+            "max_tokens" => 150,
+            "temperature" => 0.7, // Criatividade de respostas
+            "top_p" => 1.0, // Diversidade de respostas
+            "n" => 1, // Quantidade de respostas
+            "model" => "gpt-3.5-turbo-0613"
+        );
 
-    $api = curl_init($url);
-    curl_setopt($api, CURLOPT_POST, 1);
-    curl_setopt($api, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($api, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        'Authorization: Bearer ' . $apiKey,
-    ]);
+        $api = curl_init();
+        curl_setopt($api, CURLOPT_URL, $url);
+        curl_setopt($api, CURLOPT_POST, 1);
+        curl_setopt($api, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($api, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($api, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "Authorization: Bearer $apiKey"
+        ));
 
-    echo ("ChatGPT está processando sua resposta...<br><br>");
+        echo ("ChatGPT está processando sua resposta...<br><br>");
 
-    $response = curl_exec($api);
-    curl_close($api);
-    $result = json_decode($response, true);
+        $response = curl_exec($api);
+        if ($response === false) {
+            echo ("Erro na solicitação cURL: " . curl_error($api));
+        } else {
+            $result = json_decode($response, true);
 
-    if (isset($result['choices'][0]['message']['content'])) {
-        $resposta = $result['choices'][0]['message']['content'];
-        echo "Resposta: " . $resposta;
-    } else {
-        echo "Não foi possível obter uma resposta.";
+            echo ("<pre>");
+            print_r($result);
+            echo ("</pre>");
+
+            if (isset($result['choices'][0]['message']['content'])) {
+                $resposta = $result['choices'][0]['message']['content'];
+                echo ($resposta . "<br>");
+            } else {
+                echo ("Não foi possível obter uma resposta." . "<br>");
+            }
+        }
+        curl_close($api);
+    } catch (Exception $e) {
+        die("Erro: " . $e->getMessage() . "<br>");
     }
-} catch (Exception $e) {
-    die("Erro: " . $e->getMessage() . "<br>");
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OpenAI - ChatGPT</title>
+</head>
+
+<body>
+    <h2>OpenAI - ChatGPT</h2>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">        
+        <input type="text" id="pergunta" name="pergunta" placeholder="Digite sua pergunta"><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+
+    <?php
+    // Exibir a resposta aqui após o processamento
+    if (isset($resposta)) {
+        echo "<strong>Resposta:</strong> " . $resposta . "<br>";
+    }
+    ?>
+</body>
+
+</html>
--- davinci.php
+++ davinci.php
@@ -2,38 +2,75 @@
 // Chave privada e pessoal de acesso a API
 include_once("keys/apikey.php");
 
-try {
-    $entrada = "São Paulo se sagra campeão da Copa do Brasil, salve tricolor!";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    try {
+        $entrada = $_POST["pergunta"];;
 
-    // Da Vinci - Cria textos aleatorios de acordo como prompt enviado 
-    $url = 'https://api.openai.com/v1/engines/davinci/completions';
+        // Da Vinci - Cria textos aleatorios de acordo como prompt enviado 
+        $url = 'https://api.openai.com/v1/engines/davinci/completions';
 
-    $data = [
-        'prompt' => $entrada,
-        'max_tokens' => 150,
-    ];
+        $data = [
+            'prompt' => $entrada,
+            'max_tokens' => 150,
+        ];
 
-    $api = curl_init($url);
-    curl_setopt($api, CURLOPT_POST, 1);
-    curl_setopt($api, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($api, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        'Authorization: Bearer ' . $apiKey,
-    ]);
+        $api = curl_init($url);
+        curl_setopt($api, CURLOPT_POST, 1);
+        curl_setopt($api, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($api, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $apiKey,
+        ]);
 
-    echo ("Da Vinci está gerando seu texto artístico aleatório...<br><br>");
+        echo ("Da Vinci está gerando seu texto artístico aleatório...<br><br>");
 
-    $response = curl_exec($api);
-    curl_close($api);
-    $result = json_decode($response, true);
+        $response = curl_exec($api);
+        if ($response === false) {
+            echo ("Erro na solicitação cURL: " . curl_error($api));
+        } else {
+            $result = json_decode($response, true);
 
-    if (isset($result['choices'][0]['text'])) {
-        $resposta = $result['choices'][0]['text'];
-        echo ($resposta . "<br>");
-    } else {
-        echo ("Não foi possível gerar o texto!" . "<br>");
+            echo ("<pre>");
+            print_r($result);
+            echo ("</pre>");
+
+            if (isset($result['choices'][0]['text'])) {
+                $resposta = $result['choices'][0]['text'];
+                echo ($resposta . "<br>");
+            } else {
+                echo ("Não foi possível gerar o texto!" . "<br>");
+            }
+        }
+        curl_close($api);
+    } catch (Exception $e) {
+        die("Erro: " . $e->getMessage() . "<br>");
     }
-} catch (Exception $e) {
-    die("Erro: " . $e->getMessage() . "<br>");
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OpenAI - Da Vinci</title>
+</head>
+
+<body>
+    <h2>OpenAI - Da Vinci</h2>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">        
+        <input type="text" id="pergunta" name="pergunta" placeholder="Digite sua pergunta"><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+
+    <?php
+    // Exibir a resposta aqui após o processamento
+    if (isset($resposta)) {
+        echo "<strong>Resposta:</strong> " . $resposta . "<br>";
+    }
+    ?>
+</body>
+
+</html>
